a389fb9b7f4c9ca8f5490f3e7f3e10b113ef6f45
pierelucas <pierelucas@keemail.me>
2019-10-26 17:39:42 +0200

    modules

--- modules.py
+++ modules.py
@@ -12,6 +12,7 @@ Usage:
 import os
 import sys
 import requests
+from collections import deque
 
 
 class URLWorkerException():
@@ -20,46 +21,65 @@ class URLWorkerException():
 
 class URLWorker():
 
-    def __init__(self, url, *, wordlist="", wordlist_path=""):
+    def __init__(self, url, *, wordlist=(), wordlist_path=""):
         self.url = url
+        self.valid_urls = []
         self.wordlist = wordlist
         self.wordlist_path = wordlist_path
+
+    def check_subd(self):
+
+        self.worlist_creator()
+        self.check_domains()
+        return self.valid_urls
 
     def worlist_creator(self):
 
         try:
 
-            if self.wordlist == "" and self.wordlist_path == "":
+            if self.wordlist == () and self.wordlist_path == "":
                 print("Required: wordlist and/or wordlist_path argmument")
                 raise Exception
 
-            elif self.wordlist == "":
+            elif self.wordlist == ():
                 with open(self.wordlist_path, 'rt') as f:
-                    self.wordlist = f.readlines()
+                    self.wordlist = deque(f.readlines())
 
             elif self.wordlist_path == "":
-                pass
+                self.wordlist = deque(self.wordlist)
 
             else:
                 with open(self.wordlist_path, 'rt') as f:
                     wlist = f.readlines()
-                    self.wordlist = [*self.wordlist, *wlist]
+                    self.wordlist = deque([*self.wordlist, *wlist])
+
+            return
 
         except Exception as ex:
             print(ex)
             sys.exit(1)
 
     def check_domains(self):
-        pass
+
+        while self.wordlist != deque([]):
+            word = self.wordlist[0]
+            self.url = word + self.url
+            _true = self.url_online()
+            if _true:
+                self.valid_urls.append(self.url)
+            self.wordlist.popleft()
+            continue
+        return
+
 
     def url_online(self):
+
         try:
 
             url = requests.get(self.url)
             status = url.status_code
-            if status < 300: return True, status
-            else: return False, status
+            if status < 300: return True
+            else: return False
 
         except Exception as ex:
             print("Error in function 'url_online' :", ex)
-
